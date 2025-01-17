c62569319a739f898cad5f197bba840135f7bbaf
Josselrempis02 <josselrempis02@gmail.com>
2025-01-17 09:59:19 +0800

    edit index file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -90,24 +90,7 @@
         </div>
         <div class="contact1">
             <iframe data-aos="fade-right" data-aos-delay="500" class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.4238313302826!2d121.11816437586783!3d14.688607774914379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397bb9db65e6af1%3A0x1cc414e6e2fe69a4!2sKapenista!5e0!3m2!1sen!2sph!4v1708825386001!5m2!1sen!2sph" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <form data-aos="fade-left" data-aos-delay="500" action="">
-                <h3>Get in touch</h3>
-                <div class="inputbox">
-                    <span class="fas fa-user"></span>
-                    <input type="text" placeholder="Name">
-                </div>
-                <div class="inputbox">
-                    <span class="fa-solid fa-envelope"></span>
-                    <input type="email" placeholder="Email">
-                </div>
-                <div class="inputbox">
-                    <span class="fa-solid fa-message"></span>
-                    <input type="text" placeholder="Message">
-                </div>
-                <div class="order-btn">
-                    <a href="{{ url('Shop') }}" class="order-btn">Send us message</a>
-                </div>
-            </form>
+           
         </div>
     </section>
     <!--Contact-section-endss -->
